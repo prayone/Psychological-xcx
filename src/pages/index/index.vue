6a345b2b2801f0ece387b8e468479eb894f7ae6f
wangping <1054864949@qq.com>
2021-03-21 17:49:14 +0800

    新建小程序

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  methods: {},
+
+  created() {
+    // let app = getApp()
+  },
+}
+</script>
+
+<style scoped>
+</style>
